255853ca42b91e28fcb3fe18cb2ae4f5d9622d4f
Girverius <gsmac1224@gmail.com>
2020-10-28 20:20:31 -0500

    added rows and functional date

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").append(currentDate);
+    console.log(currentDate)
+    
+    $("#9AM").text("9:00AM");
+    $("#10AM").text("10:00AM");
+    $("#11AM").text("11:00AM");
+    $("#12PM").text("12:00AM");
+    $("#1PM").text("1:00PM");
+    $("#2PM").text("2:00PM");
+    $("#3PM").text("3:00PM");
+    $("#4PM").text("4:00PM");
+    $("#5PM").text("5:00PM");
+});
+
