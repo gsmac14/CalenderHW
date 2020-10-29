5963ce246d9dab7d6631ca764858ba873fb8f11c
Girverius <gsmac1224@gmail.com>
2020-10-28 21:41:10 -0500

    added some css

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+    //daily time and day
     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(currentDate);
     console.log(currentDate)
-    
+    //time
     $("#9AM").text("9:00AM");
     $("#10AM").text("10:00AM");
     $("#11AM").text("11:00AM");
@@ -12,5 +13,11 @@ $(document).ready(function () {
     $("#3PM").text("3:00PM");
     $("#4PM").text("4:00PM");
     $("#5PM").text("5:00PM");
+
+    //create text area in rows 
+
+
+
+
 });
 
