d16d10bc6b38e6994b259814d7f032d3b916a8ef
Girverius <gsmac1224@gmail.com>
2020-10-29 18:28:57 -0500

    added color to textArea

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(currentDate);
     console.log(currentDate)
+    
     //time
     $("#9AM").text("9:00AM");
     $("#10AM").text("10:00AM");
@@ -14,8 +15,31 @@ $(document).ready(function () {
     $("#4PM").text("4:00PM");
     $("#5PM").text("5:00PM");
 
-    //create text area in rows 
+ //create changing colors for past, present, and future
+$("textarea").addClass("textArea");
 
+$(".row").each(function(){
+    var time = $(this.children[0]).text();
+    console.log(time);
+    if(time < currentDate){
+        $(this.children[1]).addClass("past");
+        console.log(time);
+    }
+    else if(time > currentDate){
+        $(this.children[1]).addClass("present"); 
+    
+    }
+    else{
+        $(this.children[1]).addClass("future");
+    }
+})
+
+$(".saveBtn").on("click", function(){
+    textArea = $(".col-10").value
+
+    localStorage.setItem("col-10", textArea);
+
+});
 
 
 
