fca3398d16a69619a2d577bb20e81961fc9ef24e
Girverius <gsmac1224@gmail.com>
2020-10-31 09:25:17 -0500

    attempt to retrieve Items

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
 $(document).ready(function () {
-    //daily time and day
-    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $("#currentDay").append(currentDate);
-    // console.log(currentDate)
-    
-    //time
-    $("#9").text("9AM");
-    $("#10").text("10AM");
-    $("#11").text("11AM");
-    $("#12").text("12AM");
-    $("#13").text("1PM");
-    $("#14").text("2PM");
-    $("#15").text("3PM");
-    $("#16").text("4PM");
-    $("#17").text("5PM");
+  //daily time and day
+  var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+  $("#currentDay").append(currentDate);
+  // console.log(currentDate)
 
- //create changing colors for past, present, and future
- //used if and else statements to to compare times between past, present and future
- //
+  //time
+  $("#9").text("9AM");
+  $("#10").text("10AM");
+  $("#11").text("11AM");
+  $("#12").text("12AM");
+  $("#13").text("1PM");
+  $("#14").text("2PM");
+  $("#15").text("3PM");
+  $("#16").text("4PM");
+  $("#17").text("5PM");
 
-$(".row").each(function(){
+  //create changing colors for past, present, and future
+  //used if and else statements to to compare times between past, present and future
+
+  $(".row").each(function () {
     var time = $(this.children[0]).text();
     // console.log(time);
-    if(time < currentDate){
-        $(this.children[1]).css("background-color", "grey");
-        // console.log(time);
+    if (time < currentDate) {
+      $(this.children[1]).css("background-color", "grey");
+      // console.log(time);
+    } else if (time > currentDate) {
+      $(this.children[1]).css("background-color", "green");
+    } else {
+      $(this.children[1]).css("background-color", "red");
     }
-    else if(time > currentDate){
-        $(this.children[1]).css("background-color", "green"); 
-    
-    }
-    else{
-        $(this.children[1]).css("background-color", "red");
-    }
-})
+  });
 
-$(".saveBtn").on("click", function(){
-  var value = $(this).siblings(".col-10").val();
-  var timeEl = $(this).siblings(".time-block").attr("id")
-  localStorage.setItem(timeEl, value);
-  //console.log(value);
-
-
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".col-10").val();
+    var timeEl = $(this).siblings(".time-block").attr("id");
+    localStorage.setItem(timeEl, value);
+    //console.log(value);
+    var lastInput = localStorage.getItem(value);
+  });
 });
-
-
-
-});
-
