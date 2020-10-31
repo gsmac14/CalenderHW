b375643cce1e5a2bfc6497d57c2057637cbe1770
Girverius <gsmac1224@gmail.com>
2020-10-30 21:39:42 -0500

    localstorage added

--- script.js
+++ script.js
@@ -2,42 +2,45 @@ $(document).ready(function () {
     //daily time and day
     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(currentDate);
-    console.log(currentDate)
+    // console.log(currentDate)
     
     //time
-    $("#9AM").text("9:00AM");
-    $("#10AM").text("10:00AM");
-    $("#11AM").text("11:00AM");
-    $("#12PM").text("12:00AM");
-    $("#1PM").text("1:00PM");
-    $("#2PM").text("2:00PM");
-    $("#3PM").text("3:00PM");
-    $("#4PM").text("4:00PM");
-    $("#5PM").text("5:00PM");
+    $("#9").text("9:00AM");
+    $("#10").text("10:00AM");
+    $("#11").text("11:00AM");
+    $("#12").text("12:00AM");
+    $("#13").text("1:00PM");
+    $("#14").text("2:00PM");
+    $("#15").text("3:00PM");
+    $("#16").text("4:00PM");
+    $("#17").text("5:00PM");
 
  //create changing colors for past, present, and future
-$("textarea").addClass("textArea");
+ //used if and else statements to to compare times between past, present and future
+ //
 
 $(".row").each(function(){
     var time = $(this.children[0]).text();
-    console.log(time);
+    // console.log(time);
     if(time < currentDate){
-        $(this.children[1]).addClass("past");
-        console.log(time);
+        $(this.children[1]).css("background-color", "grey");
+        // console.log(time);
     }
     else if(time > currentDate){
-        $(this.children[1]).addClass("present"); 
+        $(this.children[1]).css("background-color", "green"); 
     
     }
     else{
-        $(this.children[1]).addClass("future");
+        $(this.children[1]).css("background-color", "red");
     }
 })
 
 $(".saveBtn").on("click", function(){
-    textArea = $(".col-10").value
+  var value = $(this).siblings(".col-10").val();
+  var timeEl = $(this).siblings(".time-block").attr("id")
+  localStorage.setItem(timeEl, value);
+  //console.log(value);
 
-    localStorage.setItem("col-10", textArea);
 
 });
 
