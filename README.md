6eec8885bbfe3321af9dc73d75cd1836fc45831e
Girverius <gsmac1224@gmail.com>
2020-10-31 22:27:26 -0500

    README added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This project entailed created a daily calender with usage of html, css, javascript, and jquery. The date and time had to be added to the top of the page with moment.js. Text entered on the calender by the user was to be saved to local storage. Lastly the calender had to show past, present, and future with coordinating colors. 
 https://gsmac14.github.io/CalenderHW/
+![Alt text](/images/planner.png)
--- script.js
+++ script.js
@@ -2,44 +2,46 @@ $(document).ready(function () {
   //daily time and day
   var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").append(currentDate);
-  // console.log(currentDate)
+ 
 
-  //time
-  $("#9").text("9AM");
+  // set time in tim-block
+  $("#9").text("  9AM");
   $("#10").text("10AM");
   $("#11").text("11AM");
   $("#12").text("12AM");
-  $("#13").text("1PM");
-  $("#14").text("2PM");
-  $("#15").text("3PM");
-  $("#16").text("4PM");
-  $("#17").text("5PM");
-  //   console.log("#9")
+  $("#13").text("  1PM");
+  $("#14").text("  2PM");
+  $("#15").text("  3PM");
+  $("#16").text("  4PM");
+  $("#17").text("  5PM");
 
 
-  //create changing colors for past, present, and future
+
+  //created changing colors for past, present, and future
   //used if and else statements to to compare times between past, present and future
 
   $(".row").each(function () {
-    var time = $(this.children[0]).text();
+    var time = $(this.children[1]).text();
     // console.log(time);
     if (time < currentDate) {
-      $(this.children[1]).css("background-color", "grey");
-      // console.log(time);
+      $(this.children[1]).addClass("past");
+
     } else if (time > currentDate) {
-      $(this.children[1]).css("background-color", "green");
+      $(this.children[1]).addClass("future");
+
     } else {
-      $(this.children[1]).css("background-color", "red");
+      $(this.children[1]).addClass("present");
     }
   });
 
+  //event listener created and alsot created local storage
   $(".saveBtn").on("click", function (event) {
     event.preventDefault();
     console.log($(this)[0].parentElement.children[0]);
     var timeId = $(this)[0].parentElement.children[0].id;
     var value = $(this).siblings(".col-10").val();
     localStorage.setItem(timeId, value);
-    //console.log(value);
+  
   });
   //creted localstarge getItme to help entered text stay after refreshed.
   for (var i = 1; i < 10; i++) {
